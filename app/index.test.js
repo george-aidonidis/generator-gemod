7df260cb096f72ee360778896dc11037fbd58177
George Aidonidis <george.aidonidis@gmail.com>
2019-02-26 19:55:55 +0200

    Fix tests

--- app/index.test.js
+++ app/index.test.js
@@ -7,106 +7,106 @@ const utils = require('./utils');
 let generator;
 
 beforeAll(async () => {
-  await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
-  generator = helpers.createGenerator('gemod:app', ['../'], null, {
-    skipInstall: true,
-    yarn: true,
-    npm: false,
-    bower: false
-  });
-  return generator;
+	await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
+	generator = await helpers.createGenerator('gemod:app', ['../'], null, {
+		skipInstall: true,
+		yarn: true,
+		npm: false,
+		bower: false,
+	});
 });
 
 describe('Mod Generator', () => {
-  it('generates expected files', async () => {
-    helpers.mockPrompt(generator, {
-      moduleName: 'test',
-      githubUsername: 'test',
-      website: 'test.com'
-    });
-    await pify(generator.run.bind(generator))();
-    assert.file([
-      '.git',
-      '.gitignore',
-      '.travis.yml',
-      'index.js',
-      'license',
-      'package.json',
-      'readme.md',
-      'index.test.js',
-      '.npmrc'
-    ]);
-  });
-  it('codecoverage option', async () => {
-    helpers.mockPrompt(generator, {
-      moduleName: 'test',
-      githubUsername: 'test',
-      website: 'test.com',
-      codecoverage: true,
-      codecov: false
-    });
+	it('generates expected files', async () => {
+		helpers.mockPrompt(generator, {
+			moduleName: 'test',
+			githubUsername: 'test',
+			website: 'test.com',
+		});
+		await pify(generator.run.bind(generator))();
+		assert.file([
+			'.git',
+			'.gitignore',
+			'.prettierrc',
+			'.travis.yml',
+			'index.js',
+			'license',
+			'package.json',
+			'readme.md',
+			'index.test.js',
+			'.npmrc',
+		]);
+	});
+	it('codecoverage option', async () => {
+		helpers.mockPrompt(generator, {
+			moduleName: 'test',
+			githubUsername: 'test',
+			website: 'test.com',
+			codecoverage: true,
+			codecov: false,
+		});
 
-    await pify(generator.run.bind(generator))();
+		await pify(generator.run.bind(generator))();
 
-    assert.fileContent('.gitignore', /coverage/);
-    assert.fileContent('package.json', /"xo && jest --coverage"/);
-    assert.noFileContent('package.json', /"codecov":/);
-    assert.noFileContent('.travis.yml', /codecov/);
-  });
+		assert.fileContent('.gitignore', /coverage/);
+		assert.fileContent('package.json', /"xo"/);
+		assert.noFileContent('package.json', /"codecov":/);
+		assert.noFileContent('.travis.yml', /codecov/);
+	});
 
-  it('codecov option', async () => {
-    helpers.mockPrompt(generator, {
-      moduleName: 'test',
-      githubUsername: 'test',
-      website: 'test.com',
-      codecoverage: true,
-      codecov: true
-    });
+	it('codecov option', async () => {
+		helpers.mockPrompt(generator, {
+			moduleName: 'test',
+			githubUsername: 'test',
+			website: 'test.com',
+			codecoverage: true,
+			codecov: true,
+		});
 
-    await pify(generator.run.bind(generator))();
+		await pify(generator.run.bind(generator))();
 
-    assert.fileContent('.gitignore', /coverage/);
-    assert.fileContent('package.json', /"xo && jest --coverage"/);
-    assert.fileContent('package.json', /"codecov":/);
-    assert.fileContent('.travis.yml', /codecov/);
-  });
+		assert.fileContent('.gitignore', /coverage/);
+		assert.fileContent('package.json', /"xo"/);
+		assert.fileContent('package.json', /"codecov":/);
+		assert.fileContent('.travis.yml', /codecov/);
+	});
 
-  it('prompts for description', async () => {
-    helpers.mockPrompt(generator, {
-      moduleName: 'test',
-      moduleDescription: 'foo',
-      githubUsername: 'test',
-      website: 'test.com',
-      nyc: true,
-      codecov: true
-    });
+	it('prompts for description', async () => {
+		helpers.mockPrompt(generator, {
+			moduleName: 'test',
+			moduleDescription: 'foo',
+			githubUsername: 'test',
+			website: 'test.com',
+			nyc: true,
+			codecov: true,
+		});
 
-    await pify(generator.run.bind(generator))();
+		await pify(generator.run.bind(generator))();
 
-    assert.fileContent('package.json', /"description": "foo",/);
-    assert.fileContent('readme.md', /> foo/);
-  });
+		assert.fileContent('package.json', /"description": "foo",/);
+		assert.fileContent('readme.md', /> foo/);
+	});
 
-  it('defaults to superb description', async () => {
-    helpers.mockPrompt(generator, {
-      moduleName: 'test',
-      githubUsername: 'test',
-      website: 'test.com',
-      nyc: true,
-      codecov: true
-    });
+	it('defaults to superb description', async () => {
+		helpers.mockPrompt(generator, {
+			moduleName: 'test',
+			githubUsername: 'test',
+			website: 'test.com',
+			nyc: true,
+			codecov: true,
+		});
 
-    await pify(generator.run.bind(generator))();
+		await pify(generator.run.bind(generator))();
 
-    assert.fileContent('package.json', /"description": "My .+ module",/);
-    assert.fileContent('readme.md', /> My .+ module/);
-  });
+		assert.fileContent('package.json', /"description": "My .+ module",/);
+		assert.fileContent('readme.md', /> My .+ module/);
+	});
 
-  it('parse scoped package names', () => {
-    expect(utils.slugifyPackageName('author/thing')).toEqual('author-thing');
-    expect(utils.slugifyPackageName('@author/thing')).toEqual('@author/thing');
-    expect(utils.slugifyPackageName('@author/hi/there')).toEqual(
-      'author-hi-there'
-    );
-  });
+	it('parse scoped package names', () => {
+		expect(utils.slugifyPackageName('author/thing')).toEqual('author-thing');
+		expect(utils.slugifyPackageName('@author/thing')).toEqual('@author/thing');
+		expect(utils.slugifyPackageName('@author/hi/there')).toEqual(
+			'author-hi-there',
+		);
+	});
 });
